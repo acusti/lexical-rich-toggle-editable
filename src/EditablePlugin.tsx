73c733eddb31a389009c8f42c0731ce9fecf2b17
acusti <andrew@acusti.ca>
2024-05-07 12:54:44 -0700

    Add focus logic to EditablePlugin

--- src/EditablePlugin.tsx
+++ src/EditablePlugin.tsx
@@ -1,12 +1,34 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import { $selectAll } from 'lexical';
 import { useEffect } from 'react';
 
 export default function EditablePlugin({ isEditable }: { isEditable: boolean }) {
     const [editor] = useLexicalComposerContext();
 
+    useEffect(() =>
+        editor.registerEditableListener((_isEditable) => {
+            if (_isEditable) {
+                // editor.focus(); // also tried focussing here with no luck
+                editor.update(() => {
+                    editor.focus();
+                    $selectAll();
+                });
+            }
+        }),
+        [editor],
+    );
+
     useEffect(() => {
         editor.setEditable(isEditable);
-        console.log('toggling isEditable', isEditable);
+        /* I first tried to focus from here, but it didn’t work, so I tried the listener
+        if (isEditable) {
+            // editor.focus(); // also tried focussing here with no luck
+            editor.update(() => {
+                editor.focus();
+                $selectAll();
+            });
+        }
+        */
     }, [editor, isEditable]);
 
     return null;
