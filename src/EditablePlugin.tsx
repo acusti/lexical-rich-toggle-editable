f9d5441c7ff95b5e9f29371ec94f9b30255ad097
acusti <andrew@acusti.ca>
2024-05-07 14:38:32 -0700

    Use useLexicalEditable + useEffect for autofocus

--- src/EditablePlugin.tsx
+++ src/EditablePlugin.tsx
@@ -1,27 +1,23 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
+import useLexicalEditable from '@lexical/react/useLexicalEditable';
 import { $selectAll } from 'lexical';
 import { useEffect } from 'react';
 
 export default function EditablePlugin({ isEditable }: { isEditable: boolean }) {
     const [editor] = useLexicalComposerContext();
-
-    useEffect(() =>
-        editor.registerEditableListener((_isEditable) => {
-            if (_isEditable) {
-                setTimeout(() => {
-                    editor.focus();
-                    editor.update(() => {
-                        $selectAll();
-                    });
-                }, 0); // next tick
-            }
-        }),
-        [editor],
-    );
+    const editableState = useLexicalEditable();
 
     useEffect(() => {
         editor.setEditable(isEditable);
     }, [editor, isEditable]);
+
+    useEffect(() => {
+        if (editableState) {
+            editor.update(() => {
+                $selectAll();
+            });
+        }
+    }, [editableState, editor]);
 
     return null;
 }
